4573097b4bc1821d5b0b7f7745abb082bed4fe0c
Florin1335 <floricelflorin2000@gmail.com>
2021-08-27 23:11:44 +0300

    email fix

--- backend/routere/autentificare.js
+++ backend/routere/autentificare.js
@@ -47,30 +47,30 @@ const colectii = [_placaVideo.default, _procesor.default, _placaDeBaza.default, 
 
 var auth = _express.default.Router();
 
-let mailjetClient = _nodeMailjet.default.connect(process.env.mailjetKey || 'ecc8f1b34b0aac72d80efe7d04359771', process.env.mailjetSecretKey || '188b938d212c6ca8fb9dbf88beb6728d');
+let mailjetClient = _nodeMailjet.default.connect(process.env.mailjetKey || "ecc8f1b34b0aac72d80efe7d04359771", process.env.mailjetSecretKey || "188b938d212c6ca8fb9dbf88beb6728d");
 
-auth.get('/', (0, _expressAsyncHandler.default)((req, res) => {
+auth.get("/", (0, _expressAsyncHandler.default)((req, res) => {
   if (req.session.isAuth === true) res.json(true);else res.json(false);
 }));
 
 const trimiteEmailConfirmare = (prenume, email, cod_activare) => {
   const request = mailjetClient.post("send", {
-    'version': 'v3.1'
+    version: "v3.1"
   }).request({
-    "Messages": [{
-      "From": {
-        "Email": "xflorinutz@gmail.com",
-        "Name": "ComponentePC"
+    Messages: [{
+      From: {
+        Email: "xflorinutz@gmail.com",
+        Name: "ComponentePC"
       },
-      "To": [{
-        "Email": email,
-        "Name": prenume
+      To: [{
+        Email: email,
+        Name: prenume
       }],
-      "Subject": "Activare cont pe site-ul ComponentePC",
-      "HTMLPart": `<h1>Confirmare cont pe site-ul ComponentePC</h1>
+      Subject: "Activare cont pe site-ul ComponentePC",
+      HTMLPart: `<h1>Confirmare cont pe site-ul ComponentePC</h1>
           <h2>Salut ${prenume},</h2>
-          <p>Pentru activarea contului dumneavoastră apăsați <a href=http://localhost:3000/activare_cont/${cod_activare}>aici.</a></p>`,
-      "CustomID": "AppGettingStartedTest"
+          <p>Pentru activarea contului dumneavoastră apăsați <a href=https://demoshoppingwebsite.herokuapp.com/activare_cont/${cod_activare}>aici.</a></p>`,
+      CustomID: "AppGettingStartedTest"
     }]
   });
   request.then(result => {
@@ -80,7 +80,7 @@ const trimiteEmailConfirmare = (prenume, email, cod_activare) => {
   });
 };
 
-auth.post('/register', (0, _expressAsyncHandler.default)(async (req, res) => {
+auth.post("/register", (0, _expressAsyncHandler.default)(async (req, res) => {
   let {
     email,
     parola,
@@ -91,11 +91,11 @@ auth.post('/register', (0, _expressAsyncHandler.default)(async (req, res) => {
   let exista_deja = await _utilizatori.default.findOne({
     email
   });
-  if (exista_deja) res.status(500).json('Adresa de email este deja înregistrată.');else {
+  if (exista_deja) res.status(500).json("Adresa de email este deja înregistrată.");else {
     const parola_criptata = await _bcrypt.default.hash(parola, 10);
 
     if (parola_criptata) {
-      let cod_random = '';
+      let cod_random = "";
       cod_random = _crypto.default.randomBytes(64).toString("hex");
       let newuser = new _utilizatori.default({
         email,
@@ -106,14 +106,14 @@ auth.post('/register', (0, _expressAsyncHandler.default)(async (req, res) => {
       });
       await newuser.save(err => {
         if (err) res.status(500).json("Eroare: " + err.message);else {
-          res.json('Utilizatorul a fost înregistrat cu succes. Accesați link-ul din email pentru activare.');
+          res.json("Utilizatorul a fost înregistrat cu succes. Accesați link-ul din email pentru activare.");
           trimiteEmailConfirmare(newuser.prenume, newuser.email, newuser.cod_activare);
         }
       });
-    } else res.status(500).json('Eroare, încercați din nou.');
+    } else res.status(500).json("Eroare, încercați din nou.");
   }
 }));
-auth.post('/activare_cont', async (req, res) => {
+auth.post("/activare_cont", async (req, res) => {
   const cod = req.body.cod_activare;
   const exista = await _utilizatori.default.findOne({
     cod_activare: cod
@@ -122,16 +122,16 @@ auth.post('/activare_cont', async (req, res) => {
   if (exista) {
     exista.activat = true;
     exista.cod_activare = null;
-    exista.markModified('activat');
-    exista.markModified('cod_activare');
+    exista.markModified("activat");
+    exista.markModified("cod_activare");
     await exista.save(err => {
-      if (err) res.status(500).json('Eroare: ' + err.message);else {
-        res.json('Contul dumneavoastră a fost activat cu succes. Veți fi redirecționați în scurt timp la pagina de autentificare.');
+      if (err) res.status(500).json("Eroare: " + err.message);else {
+        res.json("Contul dumneavoastră a fost activat cu succes. Veți fi redirecționați în scurt timp la pagina de autentificare.");
       }
     });
-  } else res.status(500).json('Contul a fost deja activat sau codul de activare nu există.');
+  } else res.status(500).json("Contul a fost deja activat sau codul de activare nu există.");
 });
-auth.post('/login', async (req, res) => {
+auth.post("/login", async (req, res) => {
   const email = req.body.email.toLowerCase();
   const parola = req.body.parola;
   const exista = await _utilizatori.default.findOne({
@@ -146,27 +146,27 @@ auth.post('/login', async (req, res) => {
         req.session.isAuth = true;
         req.session.userID = exista._id;
         req.session.email = exista.email;
-        res.status(200).send(JSON.stringify('Utilizator autentificat cu succes.'));
+        res.status(200).send(JSON.stringify("Utilizator autentificat cu succes."));
       } else {
-        res.status(403).json('Utilizator neactivat.');
+        res.status(403).json("Utilizator neactivat.");
       }
     } else {
-      res.status(401).json('Email sau parola gresită.');
+      res.status(401).json("Email sau parola gresită.");
     }
-  } else res.status(401).json('Email sau parola gresită.');
+  } else res.status(401).json("Email sau parola gresită.");
 });
-auth.delete('/logout', (req, res) => {
+auth.delete("/logout", (req, res) => {
   req.session.destroy(err => {
-    if (err) res.status(500).json(err.message);else res.json('Logout cu succes.');
+    if (err) res.status(500).json(err.message);else res.json("Logout cu succes.");
   });
 });
 
 const protectie_autentificare = async (req, res, next) => {
-  if (req.session.isAuth === true) next();else res.status(401).json('Utilizator neautentificat.');
+  if (req.session.isAuth === true) next();else res.status(401).json("Utilizator neautentificat.");
 };
 
 auth.use(protectie_autentificare);
-auth.get('/info', async (req, res) => {
+auth.get("/info", async (req, res) => {
   // user info
   const user = await _utilizatori.default.findById(req.session.userID);
 
@@ -179,9 +179,9 @@ auth.get('/info', async (req, res) => {
       adresa: user.adresa
     };
     res.json(data);
-  } else res.status(500).json('Eroare preluare date.');
+  } else res.status(500).json("Eroare preluare date.");
 });
-auth.post('/adresa/set', async (req, res) => {
+auth.post("/adresa/set", async (req, res) => {
   // setare adresă
   let email = req.session.email;
   const exista = await _utilizatori.default.findOne({
@@ -196,15 +196,15 @@ auth.post('/adresa/set', async (req, res) => {
       break;
     }
 
-    if (ok === 0) res.json('status-eroare-date-eronate');else {
+    if (ok === 0) res.json("status-eroare-date-eronate");else {
       exista.adresa = req.body;
       await exista.save(err => {
-        if (err) res.json(err.message);else res.json('status-adresa-salvata');
+        if (err) res.json(err.message);else res.json("status-adresa-salvata");
       });
     }
-  } else res.json('status-eroare-utilizatorul-nu-exista');
+  } else res.json("status-eroare-utilizatorul-nu-exista");
 });
-auth.get('/comenzi', async (req, res) => {
+auth.get("/comenzi", async (req, res) => {
   // se va returna un array ce contine comenzile
   let userID = req.session.userID;
   const exista = await _comenzi.default.find({
@@ -225,13 +225,13 @@ auth.get('/comenzi', async (req, res) => {
         if (produs) obj[produs.denumire] = exista[j].cos[i];else obj[i] = exista[j].cos[i];
       }
 
-      obj['istoric'] = exista[j].istoric;
-      obj['finalizata'] = exista[j].status === 2 ? true : false;
+      obj["istoric"] = exista[j].istoric;
+      obj["finalizata"] = exista[j].status === 2 ? true : false;
       raspuns.push(obj);
     }
   }
 
-  if (raspuns.length !== 0) res.json(raspuns);else res.status(404).json('Nici o comandă.');
+  if (raspuns.length !== 0) res.json(raspuns);else res.status(404).json("Nici o comandă.");
 });
 var _default = auth;
 exports.default = _default;
